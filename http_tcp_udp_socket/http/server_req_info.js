47701fcbf27089e9d1eba68e3b598672fcf221b0
daheige <zhuwei313@hotmail.com>
2017-04-04 13:02:44 +0800

    请求头信息

--- http_tcp_udp_socket/http/server_req_info.js
+++ http_tcp_udp_socket/http/server_req_info.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs   = require('fs');
 let server = http.createServer(function(req, res) {
     if(req.url != '/favicon.ico'){
+        console.log(req.headers); //请求头信息
         var outFile = fs.createWriteStream('req_info.log',{'flag':'w','encoding':'utf8'});
         outFile.write('客户端请求方法是'+req.method+'\n');
         outFile.write('请求地址是'+req.url+'\n');
@@ -11,5 +12,3 @@ let server = http.createServer(function(req, res) {
     res.end('请求成功');
 }).listen(3000, 'localhost'); //监听3000端口
 console.log('开始请求');
-
-
